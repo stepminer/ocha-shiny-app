cafa4261b4ee93583dafb1e7429ad857f684d298
stepminer <70709034+stepminer@users.noreply.github.com>
2024-09-27 20:01:34 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("dplyr", "ggplot2","plotly","leaflet","readr")
+my_packages = c("dplyr", "ggplot2","plotly","shiny","leaflet","readr")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
